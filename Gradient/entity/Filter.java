655f2bcdc6b66ff84552cd6d9bf24208fb81b288
Swapnali <Swapnali@10.0.0.107>
2019-02-27 23:32:36 -0800

    Filter interface

---
+++ Gradient/entity/Filter.java
@@ -0,0 +1,12 @@
+package entity;
+
+import java.util.List;
+
+/*
+	Author: Swapnali Shrikhande
+	Date Created: 27th February 2019
+*/
+
+public interface Filter {
+	List<Grade> apply (List<Grade>) throws SizeException;
+}
