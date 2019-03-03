20a42598edfb733a3480eb8eaff6a4aa11c4fc10
behnam78 <behnam78@gmail.com>
2019-03-02 19:41:35 -0800

    Undo

---
+++ Gradient/entity/TotalStrategy.java
@@ -0,0 +1,39 @@
+
+package entity;
+import java.util.List;
+import entity.Grade;
+
+/*
+ * Author: Anisha Desai
+ * */
+
+public class TotalStrategy implements GradingStrategy  {
+	double total = 0.0;
+	Grade grade;
+	
+	public TotalStrategy(){
+		
+	}
+
+	@Override
+	public Grade calculate(String key, List<Grade> grades) throws SizeException {
+		// TODO Auto-generated method stub
+		double number = 0.0;
+		
+		if(grades.equals(null) || grades.size()==0)
+			throw new SizeException("List cant be empty");
+		else {
+				for (int i = 0; i < grades.size(); i++) {
+					
+					number = Missing.doubleValue(grades.get(i).getValaue());
+						
+					total += number;
+				}
+			grade =	new Grade(key, total);		
+		}
+		return grade;
+
+		}
+	}
+
+
