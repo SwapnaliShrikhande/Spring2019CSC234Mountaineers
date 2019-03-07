79fa750d3ea602655e37cd3787cdd4a8f4ec41ee
behnam78 <behnam78@gmail.com>
2019-03-06 19:30:00 -0800

    GradientTest to test values posted by professor

--- Gradient/testing/GradientTest.java
+++ Gradient/testing/GradientTest.java
@@ -43,6 +43,30 @@ class GradientTest {
         System.out.println("\n");
         Gradient.main(args);
     }
+	@Test
+	public void testMain5() {
+        String [] args = {"NA","NA","NA","NA","NA","NA","NA","NA","NA","NA","NA","NA","NA"};
+        System.out.println("\n");
+        Gradient.main(args);
+    }
+	@Test
+	public void testMain6() {
+        String [] args = {"20","18","5","15","20","20","20","NA","5","10","15","80","75"};
+        System.out.println("\n");
+        Gradient.main(args);
+    }
+	@Test
+	public void testMain7() {
+        String [] args = {"20","18","5","15","NA","20","20","5","0","NA","15","NA","NA"};
+        System.out.println("\n");
+        Gradient.main(args);
+    }
+	@Test
+	public void testMain8() {
+        String [] args = {"NA","20","5","15","NA","NA","20","NA","0","NA","15","NA","NA"};
+        System.out.println("\n");
+        Gradient.main(args);
+    }
 
 
 }
