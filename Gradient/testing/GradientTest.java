0d3b098469759764438a161d3e09ebb67c315c89
behnam78 <behnam78@gmail.com>
2019-03-06 18:01:42 -0800

    GradientTest Changed

--- Gradient/testing/GradientTest.java
+++ Gradient/testing/GradientTest.java
@@ -17,21 +17,25 @@ class GradientTest extends Gradient {
 	@Test
 	public void testMain2() {
         String [] args = {"20","18","5","15","20","20","20","0","5","10","15","80","75"};
+        System.out.println("\n");
         Gradient.main(args);
     }
 	@Test
 	public void testMain3() {
         String [] args = {"20","18","5","15","20","20","20","0","5","10","15","80",null};
+        System.out.println("\n");
         Gradient.main(args);
     }
 	@Test
 	public void testMain4() {
         String [] args = {"20","18","5","15","20","20","20","0","5","10","15","80","start"};
+        System.out.println("\n");
         Gradient.main(args);
     }
 	@Test
 	public void testMain5() {
 		String[] args = null;
+		 System.out.println("\n");
         Gradient.main(args);
     }
 }
