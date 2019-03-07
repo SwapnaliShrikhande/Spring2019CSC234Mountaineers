4e3f463969273d6a5cfb39399616207a532b5695
Swapnali <Swapnali@10.0.0.107>
2019-03-06 22:13:16 -0800

    testCourseWeightsNull

--- Gradient/testing/WeightedTotalStrategyTest.java
+++ Gradient/testing/WeightedTotalStrategyTest.java
@@ -78,5 +78,19 @@ class WeightedTotalStrategyTest {
 		summedGrades.add(new Grade("Final", 75.0));
 		assertEquals("Course Grade:  43.5", courseStrategy.calculate("Course Grade", summedGrades).toString());
 	}
+	
+	@Test
+	public void testCourseWeightsNull() throws SizeException {
+		HashMap<String, Double> courseWeights = new HashMap<String, Double>();
+		courseWeights = null;
+		WeightedTotalStrategy courseStrategy;
+		courseStrategy = new WeightedTotalStrategy(courseWeights);
+		List<Grade> summedGrades = new ArrayList<Grade>();
+		summedGrades.add(new Grade("PAs", 93.0));
+		summedGrades.add(new Grade("HWs", 50.0));
+		summedGrades.add(new Grade("Midterm", 80.0));
+		summedGrades.add(new Grade("Final", 75.0));
+		assertEquals("Course Grade: 298.0", courseStrategy.calculate("Course Grade", summedGrades).toString());
+	}
 
 }
