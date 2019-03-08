aa7fa6f54a5b4726771084ba106f024924fbccb2
behnam78 <behnam78@gmail.com>
2019-03-07 16:31:36 -0800

    Per Professor request to add metrics

---
+++ Gradient/codemr/Gradient/html/main_report/htmlx/modeldata/overview_metric_data.js
@@ -0,0 +1,1 @@
+var CodeMR_ProjectMetrics = {"Project Name":"Gradient", "Number of Highly Problematic Classes":0, "Number of Entities":9, "Number of Problematic Classes":0, "Class Lines of Code":195, "Number of External Packages":3, "Number of Packages":2, "Number of External Entities":25}
---
+++ Gradient/codemr/Gradient/html/main_report/htmlx/modeldata/packagedependency.js
@@ -0,0 +1,8 @@
+var matrix = [[0,1],[0,0]];
+var packages = [{
+"name": " src.app", "color": " #3182bd"
+}
+,{
+"name": " entity", "color": " #6baed6"
+}
+];
